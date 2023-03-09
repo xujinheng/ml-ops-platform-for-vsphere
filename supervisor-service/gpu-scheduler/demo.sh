782a0cf2776ff918544b9c98ac3764a7cea282a4
Jinheng Xu <xjinheng@vmware.com>
2023-03-09 18:02:18 +0800

    temp

--- supervisor-service/gpu-scheduler/demo.sh
+++ supervisor-service/gpu-scheduler/demo.sh
@@ -1,7 +1,7 @@
 cd /home/jinheng/fork/ml-ops-platform-for-vsphere/supervisor-service/gpu-scheduler
 
 
-k sh tkc-for-gpu-scheduler-only-1
+k sh tkc-for-gpu-scheduler-only-2
 # replicas to 1
 kubectl apply -f workload-tkc.yaml
 k g po -n tkc-workload
@@ -9,24 +9,23 @@ k g po -n tkc-workload
 
 k sh ""
 # node to 1
-kubectl edit tkc tkc-for-gpu-scheduler-only-1 -n tkg-ns-auto
+kubectl edit tkc tkc-for-gpu-scheduler-only-2 -n tkg-ns-auto
 
 docker build -t projects.registry.vmware.com/kubeflow/gpu-scheduler:0.2 .
 docker push projects.registry.vmware.com/kubeflow/gpu-scheduler:0.2
 kubectl delete -f gpu-scheduler.yaml 
 kubectl apply -f gpu-scheduler.yaml
 k g po -n gpu-scheduler
-kubectl logs $( kubectl get pod -n gpu-scheduler -l component=gpu-scheduler --no-headers -o custom-columns=':metadata.name' )
+kubectl logs $( kubectl get pod -n gpu-scheduler -l component=gpu-scheduler --no-headers -o custom-columns=':metadata.name' ) -n gpu-scheduler
 
 
-k sh tkc-for-gpu-scheduler-only-1
-# replicas to 2
+# for replicas 2, 0
+k sh tkc-for-gpu-scheduler-only-2
 kubectl apply -f workload-tkc.yaml
 k g po -n tkc-workload
 k d po nvidia-plugin-test-dff8ffc95-lnlhr
 
-
 k sh ""
-kubectl logs $( kubectl get pod -n gpu-scheduler -l component=gpu-scheduler --no-headers -o custom-columns=':metadata.name' )
-k g tkc tkc-for-gpu-scheduler-only-1 -n tkg-ns-auto -o
+kubectl logs $( kubectl get pod -n gpu-scheduler -l component=gpu-scheduler --no-headers -o custom-columns=':metadata.name' ) -n gpu-scheduler --follow
+k g tkc tkc-for-gpu-scheduler-only-2 -n tkg-ns-auto -o
 k g vm -n tkg-ns-auto
--- supervisor-service/gpu-scheduler/scheduler.py
+++ supervisor-service/gpu-scheduler/scheduler.py
@@ -21,17 +21,88 @@ def read_config():
 def get_tkc_secret(tkc):
     kubeconfig_path = os.path.join(workdir, 'kubeconfig.yaml')
     command = "kubectl get secret {}-kubeconfig -n {}".format(tkc['tkcName'], tkc['tkcNamespace']) + " -o jsonpath='{.data.value}'"
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
     base64.b64decode(output)
     with open(kubeconfig_path, 'a') as f:
         f.write(base64.b64decode(output).decode())
     return kubeconfig_path
 
+def query_gpu_demand(kubeconfig_path):
+    command = "kubectl get pods -A -o yaml --kubeconfig=" + kubeconfig_path
+    output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
+    pods = yaml.load(output.decode(), Loader=yaml.FullLoader)
+    GPU_demand = []
+    for index, pod in enumerate(pods['items']):
+        if pod['status']['phase'] == "Pending" and "gpu-scheduled" not in pod['metadata']['annotations']:
+            for condition in pod['status']['conditions']:
+                if condition['type'] == 'PodScheduled' and condition['status'] == 'False' and condition['reason'] == 'Unschedulable' and \
+                    re.search("0/[0-9]+ nodes are available:.*?[0-9]+ Insufficient nvidia.com/gpu", condition['message']):
+                        GPU_demand_pod_item = {}
+                        GPU_demand_pod_item['namespace'] = pod['metadata']['namespace']
+                        GPU_demand_pod_item['name'] = pod['metadata']['name']
+                        GPU_demand_pod_item['GPU_product'] = pod['spec']['nodeSelector']['nvidia.com/gpu.product']
+                        if len(pod['spec']['containers']) >= 2:
+                            GPU_demand_pod_item['GPU_count'] = functools.reduce(lambda a, b: int(a['resources']['limits']['nvidia.com/gpu']) + int(b['resources']['limits']['nvidia.com/gpu']), pod['spec']['containers'])
+                        else:
+                            GPU_demand_pod_item['GPU_count'] = pod['spec']['containers'][0]['resources']['limits']['nvidia.com/gpu']
+                        GPU_demand.append(GPU_demand_pod_item)
+    return GPU_demand
+
+def compute_GPU_node_demand(GPU_demand):
+    if len(GPU_demand) == 0:
+        return None
+    GPU_node_demand = {} 
+    for gpu_demand_item in GPU_demand:
+        if gpu_demand_item['GPU_product'] in GPU_node_demand:
+            GPU_node_demand[gpu_demand_item['GPU_product']] += int(gpu_demand_item['GPU_count'])
+        else:
+            GPU_node_demand[gpu_demand_item['GPU_product']] = int(gpu_demand_item['GPU_count'])
+    return GPU_node_demand
+
+def patch_tkc(GPU_product, GPU_count, tkc):
+    GPU_product_map = {'GRID-V100-4C': 'vgpu-v100-4c', 
+                        'GRID-V100-8C': 'vgpu-v100-8c',
+                    }
+    command = "kubectl get tkc {} -n {} -o yaml".format(tkc['tkcName'], tkc['tkcNamespace'])
+    output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
+    tkc_yaml = yaml.load(output.decode(), Loader=yaml.FullLoader)
+    nodePool_template = {
+        "name": "np-" + GPU_product_map[GPU_product],
+        "replicas": GPU_count,
+        "storageClass": "k8s-storage-policy",
+        "taints": [{"effect": "NoSchedule", "key": "nvidia.com/gpu", "value": "gpu-scheduler"}],
+        "tkr": {"reference": {"name": "v1.23.8---vmware.2-tkg.2-zshippable"}},
+        "vmClass": GPU_product_map[GPU_product],
+        "volumes":[
+            {"capacity": {"storage": "70Gi"}, "mountPath": "/var/lib/containerd", "name": "containerd"},
+            {"capacity": {"storage": "70Gi"}, "mountPath": "/var/lib/kubelet", "name": "kubelet"},
+        ]
+    }
+    while list(filter(lambda d: d['name'] == nodePool_template['name'], tkc_yaml['spec']['topology']['nodePools'])):
+        nodePool_template['name'] = nodePool_template['name'] + "1"
+    tkc_yaml['spec']['topology']['nodePools'].append(nodePool_template)
+    tkc_yaml_patch = {
+        "spec": {
+            "topology": {
+                "nodePools": tkc_yaml['spec']['topology']['nodePools']
+            }
+        }
+    }
+    with open(os.path.join(workdir, 'tkc-patch-file.yaml'), 'w') as f:
+        yaml.dump(tkc_yaml_patch, f, default_flow_style=False, allow_unicode=True)
+    command = "kubectl patch tkc {} -n {} --type merge --patch-file {}".format(tkc['tkcName'], tkc['tkcNamespace'], os.path.join(workdir, "tkc-patch-file.yaml"))
+    output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
+    return output
+
+def annotate_pod(GPU_demand, kubeconfig_path):
+    for GPU_demand_item in GPU_demand:
+        command = "kubectl annotate pod {} -n {} gpu-scheduled=True --kubeconfig={}".format(GPU_demand_item['name'], GPU_demand_item['namespace'], kubeconfig_path)
+        output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
+        print(output, error)
+
 def query_gcdm():
     command = os.path.join(workdir, 'dcgm_query.sh')
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
     output_array = output.decode().split("\n")
     gcdm_info=[]
     for line in output_array:
@@ -52,82 +123,51 @@ def query_gcdm():
             })
     return gcdm_info
 
-def query_gpu_demand(kubeconfig_path):
-    command = "kubectl get pods -A -o yaml --kubeconfig=" + kubeconfig_path
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    pods = yaml.load(output.decode(), Loader=yaml.FullLoader)
-    GPU_demand = []
-    for index, pod in enumerate(pods['items']):
-        if pod['status']['phase'] == "Pending" and "gpu-scheduled" not in pod['metadata']['annotations']:
-            for condition in pod['status']['conditions']:
-                if condition['type'] == 'PodScheduled' and condition['status'] == 'False' and condition['reason'] == 'Unschedulable' and \
-                    re.search("0/[0-9]+ nodes are available:.*?[0-9]+ Insufficient nvidia.com/gpu", condition['message']):
-                        GPU_demand_pod_item = {}
-                        GPU_demand_pod_item['namespace'] = pod['metadata']['namespace']
-                        GPU_demand_pod_item['name'] = pod['metadata']['name']
-                        GPU_demand_pod_item['GPU_product'] = pod['spec']['nodeSelector']['nvidia.com/gpu.product']
-                        if len(pod['spec']['containers']) >= 2:
-                            GPU_demand_pod_item['GPU_count'] = functools.reduce(lambda a, b: int(a['resources']['limits']['nvidia.com/gpu']) + int(b['resources']['limits']['nvidia.com/gpu']), pod['spec']['containers'])
-                        else:
-                            GPU_demand_pod_item['GPU_count'] = pod['spec']['containers'][0]['resources']['limits']['nvidia.com/gpu']
-                        GPU_demand.append(GPU_demand_pod_item)
-    return GPU_demand
-
-def compute_GPU_node_demand(GPU_demand, GPU_supply):
-    if len(GPU_demand) == 0:
-        return None
-    GPU_node_demand = {} 
-    a = [{'namespace': 'tkc-workload', 'name': 'nvidia-plugin-test-dff8ffc95-qrm2h', 'GPU_product': 'GRID-V100-4C', 'GPU_count': '1'}, {'namespace': 'tkc-workload', 'name': 'nvidia-plugin-test-dff8ffc95-qrm2h', 'GPU_product': 'GRID-V100-4C', 'GPU_count': '3'}, {'namespace': 'tkc-workload', 'name': 'nvidia-plugin-test-dff8ffc95-qrm2h', 'GPU_product': 'GRID-V100-8C', 'GPU_count': '2'}]
-    for gpu_demand_item in GPU_demand:
-        if gpu_demand_item['GPU_product'] in GPU_node_demand:
-            GPU_node_demand[gpu_demand_item['GPU_product']] += int(gpu_demand_item['GPU_count'])
+def get_GPU_node_info(kubeconfig_path, GPU_supply):
+    empty_nodes, occupied_nodes = [], []
+    for gpu_supply_item in GPU_supply:
+        command = "kubectl get pod {} -n gpu-operator".format(gpu_supply_item["Hostname"]) + " -o jsonpath='{.spec.nodeName}'" + " --kubeconfig={}".format(kubeconfig_path)
+        node_name, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
+        node_name = node_name.decode().strip("'")
+        if gpu_supply_item["pod"]:
+            occupied_nodes.append(node_name)
         else:
-            GPU_node_demand[gpu_demand_item['GPU_product']] = int(gpu_demand_item['GPU_count'])
-    return GPU_node_demand
+            empty_nodes.append(node_name)
+    return empty_nodes, occupied_nodes
 
-def patch_tkc(GPU_product, GPU_count, tkc):
-        GPU_product_map = {'GRID-V100-4C': 'vgpu-v100-4c', 
-                           'GRID-V100-8C': 'vgpu-v100-8c',
-                        }
-        command = "kubectl get tkc {} -n {} -o yaml".format(tkc['tkcName'], tkc['tkcNamespace'])
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        tkc_yaml = yaml.load(output.decode(), Loader=yaml.FullLoader)
-        nodePool_template = {
-            "name": "np-" + GPU_product_map[GPU_product],
-            "replicas": GPU_count,
-            "storageClass": "k8s-storage-policy",
-            "tkr": {"reference": {"name": "v1.23.8---vmware.2-tkg.2-zshippable"}},
-            "vmClass": GPU_product_map[GPU_product],
-            "volumes":[
-                {"capacity": {"storage": "70Gi"}, "mountPath": "/var/lib/containerd", "name": "containerd"},
-                {"capacity": {"storage": "70Gi"}, "mountPath": "/var/lib/kubelet", "name": "kubelet"},
-            ]
-        }
-        while list(filter(lambda d: d['name'] == nodePool_template['name'], tkc_yaml['spec']['topology']['nodePools'])):
-            nodePool_template['name'] = nodePool_template['name'] + "1"
-        tkc_yaml['spec']['topology']['nodePools'].append(nodePool_template)
-        tkc_yaml_patch = {
-            "spec": {
-                "topology": {
-                    "nodePools": tkc_yaml['spec']['topology']['nodePools']
-                }
-            }
-        }
-        with open(os.path.join(workdir, 'tkc-patch-file.yaml'), 'w') as f:
-            yaml.dump(tkc_yaml_patch, f, default_flow_style=False, allow_unicode=True)
-        command = "kubectl patch tkc {} -n {} --type merge --patch-file {}".format(tkc['tkcName'], tkc['tkcNamespace'], os.path.join(workdir, "tkc-patch-file.yaml"))
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        return output
+def destroy_node(node_item):
+    print("destroy " + node_item)
 
-def annotate_pod(GPU_demand, kubeconfig_path):
-    for GPU_demand_item in GPU_demand:
-        command = "kubectl annotate pod {} -n {} gpu-scheduled=True --kubeconfig={}".format(GPU_demand_item['name'], GPU_demand_item['namespace'], kubeconfig_path)
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        print(output, error)
+def node_destroy_countdown(kubeconfig_path, node_item, empty=True):
+    print(node_item)
+    ANNOTATION_KEY="node-destroy-countdown"
+    COUNT_DOWN = 10
+    command = "kubectl get node {} -o yaml --kubeconfig={}".format(node_item, kubeconfig_path)
+    output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
+    node_yaml = yaml.load(output.decode(), Loader=yaml.FullLoader)
+    # filter out non GPU_dedicated nodes
+    GPU_dedicated_bool = "taints" in node_yaml['spec'] and list(filter(lambda d: d['key'] == "nvidia.com/gpu", node_yaml['spec']['taints']))
+    if not GPU_dedicated_bool:
+        print("Skip the pod for not detecting taints nvidia.com/gpu")
+        return
+    # new count down value
+    annotation_exist_bool = ANNOTATION_KEY in node_yaml['metadata']['annotations']
+    print("GPU_dedicated_bool " + str(GPU_dedicated_bool))
+    print("annotation_exist_bool " + str(annotation_exist_bool))
+    print("empty " + str(empty))
+    if not empty or not annotation_exist_bool:
+        print("set new_count_down to " + str(COUNT_DOWN))
+        new_count_down = COUNT_DOWN
+    else:
+        print("new_count_down minus 1")
+        new_count_down = int(node_yaml['metadata']['annotations'][ANNOTATION_KEY]) - 1
+    print("new_count_down_value: " + str(new_count_down))
+    # destroy or annotate
+    if new_count_down == 0:
+        destroy_node(node_item)
+    else:
+        command = "kubectl annotate node {} node-destroy-countdown={} --overwrite --kubeconfig={}".format(node_item, new_count_down, kubeconfig_path)
+        output, error = subprocess.Popen(command.split(), stdout=subprocess.PIPE).communicate()
 
 config = read_config()
 print("Read Config file:")
@@ -138,34 +178,42 @@ while True:
     print("\n" + "=" * 30)
     print('%s'%time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) 
     for tkc in config['tkc']:
+        # Assumption: GPU_count for GPU_demand is always 1
         print("\n" + "-" * 15)
-        print("1. Get kubeconfig for tkc {} -n {}".format(tkc["tkcName"], tkc["tkcNamespace"]))
+        print("0. Get kubeconfig for tkc {} -n {}".format(tkc["tkcName"], tkc["tkcNamespace"]))
         kubeconfig_path = get_tkc_secret(tkc)
-        print("2. Get GPU demand")
+        print("1.1. Get GPU demand")
         GPU_demand = query_gpu_demand(kubeconfig_path)
         print(GPU_demand)
-        print("3. Get GPU GPU_supply")
-        GPU_supply = query_gcdm()
-        print(GPU_supply)
-        # Assumption: GPU_count for GPU_demand is always 1, which leads to two cases
-        # #1: GPU_demands is empty list, nothing to do
-        # #2: GPU_demands is not empty, while GPU_supply is fully used.
-        print("4. Compute GPU node demand")
-        GPU_node_demand = compute_GPU_node_demand(GPU_demand, GPU_supply)
+        print("1.2. Compute GPU node demand")
+        GPU_node_demand = compute_GPU_node_demand(GPU_demand)
         print(GPU_node_demand)
+        print("1.3. Add GPU nodes if necessary")
         if GPU_node_demand:
             print("* GPU_node_demand exist:")
-            print("5. Patch tkc")
+            print("1.4. Patch TKC")
             for GPU_product, GPU_count in GPU_node_demand.items():
-                print("Add {} {} for tkc {} -n {}".format(GPU_count, GPU_product, tkc["tkcName"], tkc["tkcNamespace"]))
+                print("Add {} {} for TKC {} -n {}".format(GPU_count, GPU_product, tkc["tkcName"], tkc["tkcNamespace"]))
                 output = patch_tkc(GPU_product, GPU_count, tkc)
                 print(output)
-            print("6. Annotate gpu-scheduled=True to scheduled pods")
+            print("1.5. Annotate gpu-scheduled=True to scheduled pods")
             print(GPU_demand)
             annotate_pod(GPU_demand, kubeconfig_path)
         else:
             print("* GPU_node_demand is empty, nothing to do")
-    print("sleep for 30sec")
+
+        print("2.1 Get GPU GPU_supply")
+        GPU_supply = query_gcdm()
+        print(GPU_supply)
+        print("2.2 Get empty/occupied GPU nodes")
+        empty_nodes, occupied_nodes = get_GPU_node_info(kubeconfig_path, GPU_supply)
+        print(empty_nodes)
+        print(occupied_nodes)
+        print("2.3 Annotate nodes with destroy countdown")
+        [node_destroy_countdown(kubeconfig_path, node_item, True) for node_item in empty_nodes]
+        [node_destroy_countdown(kubeconfig_path, node_item, False) for node_item in occupied_nodes]
+
+    print("\nsleep for 30sec")
     time.sleep(30)
     
 
@@ -194,3 +242,6 @@ while True:
 #     type: PodScheduled
 #   phase: Pending
 #   qosClass: BestEffort
+
+
+# kubectl port-forward pod/nvidia-dcgm-exporter-nbqjc 8080:9400
